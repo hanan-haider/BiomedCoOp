828cc715a2db5cba007c95fef72a4805c2dadb8a
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2025-12-24 14:57:15 +0500

    update

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import argparse
 import torch
 
-from Dassl.dassl.utils import setup_logger, set_random_seed, collect_env_info
+from dassl.utils import setup_logger, set_random_seed, collect_env_info
 from dassl.config import get_cfg_default
 from dassl.engine import build_trainer
 
